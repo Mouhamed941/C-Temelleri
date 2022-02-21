87ef64cf74e33b9225ba527a6e5c350602269b59
Mouhamed <fardosi1994@gmail.com>
2022-02-21 14:17:52 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+byte a = 254;       // 1 byte
+sbyte b = 127;      // 1 byte
+short c = 32767;    // 2 byte
+ushort d = 65535;   // 2 byte
+int e = 2147483647; // 4 byte
+Int32 h = 5;        // 4 byte
+Int64 g = 5;        // 8 byte
+long k = 5;         // 8 byte     
+char l = 'A';       // 8 byte
+string str = "Mouhamed"; // sınırsız
+DateTime tarih = DateTime.Today;
+string str1 = string.Empty;
+bool bool1 = 10<2 ;
+Console.WriteLine(bool1);
+Console.WriteLine(tarih);
